388f66485f0aeeb59ee425c8661603c07513116c
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2020-11-02 19:21:34 +0100

    Build scripts

--- buildRun.ps1
+++ buildRun.ps1
@@ -2,5 +2,5 @@ tsc
 Copy-Item -Force -Recurse -Path $PSScriptRoot\engine\parser.def -Destination $PSScriptRoot\build\engine\parser.def
 Copy-Item -Force -Recurse -Path $PSScriptRoot\playbooks\ -Destination $PSScriptRoot\build
 Copy-Item -Force -Recurse -Path $PSScriptRoot\environments\ -Destination $PSScriptRoot\build
-Copy-Item -Force -Recurse -Exclude *.ts -Path $PSScriptRoot\runners\ -Destination $PSScriptRoot\build
+Copy-Item -Force -Recurse -Path $PSScriptRoot\runners\ -Destination $PSScriptRoot\build
 node $PSScriptRoot\build\engine\run.js
---
+++ buildRun.sh
@@ -0,0 +1,6 @@
+tsc
+cp engine/parser.def build/engine/parser.def
+cp -r playbooks build
+cp -r environments build
+cp -r runners build
+node build/engine/run.js
