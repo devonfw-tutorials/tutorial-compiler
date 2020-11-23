22e4c2e98a8c9c1c078de26049c8c1b8e059c8a4
SchettlerKoehler <62466958+SchettlerKoehler@users.noreply.github.com>
2020-11-23 20:41:37 +0100

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf playbooks || true;
-(if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks; fi || git clone https://github.com/devonfw-forge/tutorials.git playbooks) && cd playbooks && (git checkout ${GITHUB_HEAD_REF} || git checkout ${GITHUB_BASE_REF} || true) && git status && git config --list
+(if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then (git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks) fi || git clone https://github.com/devonfw-forge/tutorials.git playbooks) && cd playbooks && (git checkout ${GITHUB_HEAD_REF} || git checkout ${GITHUB_BASE_REF} || true) && git status && git config --list
