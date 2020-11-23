7a258c7dd23aee7ece59bc6d642aa6102e1c547f
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2020-11-23 18:51:18 +0100

    Clone fork

--- checkout.sh
+++ checkout.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-env
-
-git clone https://github.com/devonfw-forge/tutorials.git playbooks
+rm -rf playbooks || true
+(
+    git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks 
+    || git clone https://github.com/devonfw-forge/tutorials.git playbooks
+) 
+&& cd playbooks 
+&& (
+    git chechout ${GITHUB_HEAD_REF}
+    || git checkout ${GITHUB_BASE_REF}
+    || true
+)
+&& git status 
+&& git config --list
