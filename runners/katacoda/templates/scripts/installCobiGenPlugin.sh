560f9cbbf419b74a6fab82122fe1e212d2d82dec
Julian Günther <julian.guenther@capgemini.com>
2020-11-24 12:22:07 +0100

    changed script for installing cobigen plugin

--- runners/katacoda/templates/scripts/installCobiGenPlugin.sh
+++ runners/katacoda/templates/scripts/installCobiGenPlugin.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-apt install libarchive-tools -y
 cd /root/setup/
-bsdtar -xvf <%= vsixFile; %> - extension
-mv extension /opt/.katacodacode/extensions/<%= pluginName; %>
+
+curl https://api.github.com/repos/devonfw-forge/cobigen-vscode-plugin/releases/latest |
+	grep '"browser_download_url":' |
+    sed -E 's/.*"([^"]+)".*/\1/' >> download_url.txt
+curl $(cat download_url.txt) -L --output cobigen-plugin.vsix
+
+apt install libarchive-tools -y
+bsdtar -xvf cobigen-plugin.vsix - extension
+mv extension /opt/.katacodacode/extensions/cobigen
