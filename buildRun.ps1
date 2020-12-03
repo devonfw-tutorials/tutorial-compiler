2d505c9a7ecb979f1dc4a4f3ff3ab62c9d70a77c
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2020-12-03 11:14:12 +0100

    npm test in buildRun.sh

--- buildRun.ps1
+++ buildRun.ps1
@@ -3,5 +3,4 @@ Copy-Item -Force -Recurse -Path $PSScriptRoot\engine\parser.def -Destination $PS
 Copy-Item -Force -Recurse -Path $PSScriptRoot\playbooks\ -Destination $PSScriptRoot\build
 Copy-Item -Force -Recurse -Path $PSScriptRoot\environments\ -Destination $PSScriptRoot\build
 Copy-Item -Force -Recurse -Path $PSScriptRoot\runners\ -Destination $PSScriptRoot\build
-npm test
 node $PSScriptRoot\build\engine\run.js
