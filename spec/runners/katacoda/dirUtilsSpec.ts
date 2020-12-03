eac509baf0e7efbd2cb54501cb6862e5906f3db4
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2020-12-03 11:53:24 +0100

    jasmine without @types and dirUtilsSpec changed

--- spec/runners/katacoda/dirUtilsSpec.ts
+++ spec/runners/katacoda/dirUtilsSpec.ts
@@ -1,24 +1,24 @@
-import 'jasmine'; 
+import * as jasmine from 'jasmine'; 
 import * as path from 'path';
 import { DirUtils } from '../../../runners/katacoda/dirUtils';
 
-describe("DirUtils", () => {
+jasmine.describe("DirUtils", () => {
   let target = new DirUtils();
-  describe("changeCurrentDir", () => {
-    it("is already in the right folder", () => {
-      expect(target.getCdParam(path.join('/root'), path.join('/root'))).toBe('');
+  jasmine.describe("changeCurrentDir", () => {
+    jasmine.it("is already in the right folder", () => {
+      jasmine.expect(target.getCdParam(path.join('/root'), path.join('/root'))).toBe('');
     });
-    it("changes directly to the child folder, because currentDir is the prefix of dir", () => {
-      expect(target.getCdParam(path.join('/root/devonfw'), path.join('/root/devonfw/setup'))).toBe(path.join('setup'));
+    jasmine.it("changes directly to the child folder, because currentDir is the prefix of dir", () => {
+      jasmine.expect(target.getCdParam(path.join('/root/devonfw'), path.join('/root/devonfw/setup'))).toBe(path.join('setup'));
     });
-    it("returns an absolute path, because both dirs don't have matching parent folders", () => {
-      expect(target.getCdParam(path.join('/setup'), path.join('/root/devonfw/setup'))).toBe(path.join('/root/devonfw/setup'));
+    jasmine.it("returns an absolute path, because both dirs don't have matching parent folders", () => {
+      jasmine.expect(target.getCdParam(path.join('/setup'), path.join('/root/devonfw/setup'))).toBe(path.join('/root/devonfw/setup'));
     });
-    it("changes to parent folder before changing to child folder", () => {
-      expect(target.getCdParam(path.join('/root/devonfw'), path.join('/root/setup/folder0/folder1'))).toBe(path.join('../setup/folder0/folder1'));
+    jasmine.it("changes to parent folder before changing to child folder", () => {
+      jasmine.expect(target.getCdParam(path.join('/root/devonfw'), path.join('/root/setup/folder0/folder1'))).toBe(path.join('../setup/folder0/folder1'));
     });
-    it("changes to parent folder before changing to child folder and one child folder has the same position and name", () => {
-      expect(target.getCdParam(path.join('/root/devonfw/folder/setup'), path.join('/root/devonfw/setup/setup'))).toBe(path.join('../../setup/setup'));
+    jasmine.it("changes to parent folder before changing to child folder and one child folder has the same position and name", () => {
+      jasmine.expect(target.getCdParam(path.join('/root/devonfw/folder/setup'), path.join('/root/devonfw/setup/setup'))).toBe(path.join('../../setup/setup'));
     });
 
   });
