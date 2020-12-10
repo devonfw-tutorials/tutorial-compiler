eb8fb39905de36ae631c2b968a7c1576f7d74098
Julian Günther <julian.guenther@capgemini.com>
2020-12-10 19:57:01 +0100

    changed windowsMail.yml

--- checkout.ps1
+++ checkout.ps1
@@ -5,3 +5,13 @@ Write-Output "$env:GITHUB_EVENT_NAME"
 Write-Output "$env:GITHUB_ACTOR"
 Write-Output "$env:GITHUB_HEAD_REF"
 Write-Output "$env:GITHUB_BASE_REF"
+
+if($env:GITHUB_EVENT_NAME -match "pull_request") {
+    (git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks) -or (git clone https://github.com/devonfw-forge/tutorials.git playbooks)
+} else {
+    git clone https://github.com/devonfw-forge/tutorials.git playbooks
+}
+Set-Location playbooks
+(git checkout ${GITHUB_HEAD_REF}) -or (git checkout ${GITHUB_BASE_REF})
+git status
+git config --list
