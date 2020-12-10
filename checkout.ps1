75bdcdedfd6138498489126b262a7ea3de012465
Julian Günther <julian.guenther@capgemini.com>
2020-12-10 19:08:04 +0100

    changed windowsMail.yml

--- checkout.ps1
+++ checkout.ps1
@@ -1,1 +1,7 @@
-echo "checkout.ps1"
+if(Test-Path "playbooks") {
+    Remove-Item -Recurse "playbooks"
+}
+Write-Output $GITHUB_EVENT_NAME
+Write-Output ${GITHUB_ACTOR}
+Write-Output ${GITHUB_HEAD_REF}
+Write-Output ${GITHUB_BASE_REF}
