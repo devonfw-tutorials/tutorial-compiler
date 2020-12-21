cb7b3d0b523c87105ff070a0864fec171c807aa1
Julian Günther <julian.guenther@capgemini.com>
2020-12-21 17:35:25 +0100

    changed checkout.ps1 to prevent error in windows build pipeline

--- checkout.ps1
+++ checkout.ps1
@@ -1,9 +1,13 @@
 if(Test-Path "playbooks") {
-    Remove-Item -Recurse "playbooks"
+    Get-ChildItem "playbooks" -Recurse | Remove-Item -Recurse -Force
+    Get-Item "playbooks" | Remove-Item -Recurse -Force
 }
 
 if($env:GITHUB_EVENT_NAME -match "pull_request") {
-    (git clone https://github.com/$env:GITHUB_ACTOR/tutorials.git playbooks) -or (git clone https://github.com/devonfw-forge/tutorials.git playbooks)
+    git clone https://github.com/$env:GITHUB_ACTOR/tutorials.git playbooks
+    if ( $? -eq $False ) {
+        git clone https://github.com/devonfw-forge/tutorials.git playbooks
+    }
 } else {
     git clone https://github.com/devonfw-forge/tutorials.git playbooks
 }
