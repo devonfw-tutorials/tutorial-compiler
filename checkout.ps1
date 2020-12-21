20533db6e9574aefabe82678169438310db4e9c4
Julian Günther <julian.guenther@capgemini.com>
2020-12-21 20:30:39 +0100

    added file to configure url to tutorials repo for build workflows

--- checkout.ps1
+++ checkout.ps1
@@ -3,13 +3,14 @@ if(Test-Path "playbooks") {
     Get-Item "playbooks" | Remove-Item -Recurse -Force
 }
 
+$TUTORIAL_REPO_URL=(Get-Content .\config.properties | Select-String "tutorials_repo_url").ToString().Split("=")[1]
 if($env:GITHUB_EVENT_NAME -match "pull_request") {
     git clone https://github.com/$env:GITHUB_ACTOR/tutorials.git playbooks
     if ( $? -eq $False ) {
-        git clone https://github.com/devonfw-forge/tutorials.git playbooks
+        git clone $TUTORIAL_REPO_URL playbooks
     }
 } else {
-    git clone https://github.com/devonfw-forge/tutorials.git playbooks
+    git clone $TUTORIAL_REPO_URL playbooks
 }
 Set-Location playbooks
 (git checkout $env:GITHUB_HEAD_REF) -or (git checkout $env:GITHUB_BASE_REF)
--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rm -rf playbooks || true;
-if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then (git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks || git clone https://github.com/devonfw-forge/tutorials.git playbooks) else (git clone https://github.com/devonfw-forge/tutorials.git playbooks) fi && cd playbooks && (git checkout ${GITHUB_HEAD_REF} || git checkout ${GITHUB_BASE_REF} || true) && git status && git config --list
+TUTORIAL_REPO_URL=$(cat config.properties | grep tutorials_repo_url | cut -d "=" -f2)
+if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then (git clone https://github.com/${GITHUB_ACTOR}/tutorials.git playbooks || git clone $TUTORIAL_REPO_URL playbooks) else (git clone $TUTORIAL_REPO_URL playbooks) fi && cd playbooks && (git checkout ${GITHUB_HEAD_REF} || git checkout ${GITHUB_BASE_REF} || true) && git status && git config --list
