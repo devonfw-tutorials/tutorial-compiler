0975a6eab39ad594f03c076c963ab028643e0004
Dießner <marcel.diessner@capgemini.com>
2020-12-22 13:46:27 +0100

    katacoda runner for cloneRepository implemented

--- runners/katacoda/index.ts
+++ runners/katacoda/index.ts
@@ -209,6 +209,21 @@ export class Katacoda extends Runner {
 
     }
 
+    runCloneRepository(step: Step, command: Command): RunResult {
+
+        let cdCommand = this.changeCurrentDir(path.join("/root", "devonfw", "workspaces", "main"));
+        let workspaceDir = path.join("devonfw", "workspaces", "main");
+        let directoryPath = path.join(workspaceDir, command.parameters[0]).replace(/\\/g, "/");
+
+        this.steps.push({
+            "title": "Clones Repository " + command.parameters[1],
+            "text": "step" + this.stepsCount + ".md"
+        });
+
+        this.renderTemplate("cloneRepository.md", this.outputPathTutorial + "step" + (this.stepsCount++) + ".md", { text: step.text, textAfter: step.textAfter, cdCommand: cdCommand, directoryPath: directoryPath, repository: command.parameters[1]});
+        return null;
+    }
+
     private renderTemplate(name: string, targetPath: string, variables) {
         let template = fs.readFileSync(path.join(this.getRunnerDirectory(),"templates", name), 'utf8');
         let result = ejs.render(template, variables);
---
+++ runners/katacoda/templates/cloneRepository.md
@@ -0,0 +1,15 @@
+<%= text; %>
+
+<%= cdCommand; %>
+
+If the parent directories aren't already in the project, 'mkdir -p' will create them for you. 
+
+`mkdir -p <%= directoryPath; %>`{{execute}}
+
+`cd  <%= directoryPath; %>`{{execute}}
+
+Now clone the repository to your local directory.
+
+`git clone <%= repository; %>`{{execute}}
+
+<%= textAfter; %>
