70b7ee30d31d413969841d61098c822e541f862b
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2021-02-22 10:19:47 +0100

    removed --no-save

--- runners/console/index.ts
+++ runners/console/index.ts
@@ -253,7 +253,6 @@ export class Console extends Runner {
         this.executeCommandSync("git clone " + command.parameters[1], directorypath, result);
 
         return result;
-
     }
 
     runNpmInstall(step: Step, command: Command): RunResult {
@@ -263,7 +262,7 @@ export class Console extends Runner {
         let projectPath = path.join(this.getVariable(this.workspaceDirectory), command.parameters[0]);
         let npmCommand = "npm install";
         if(command.parameters.length > 1){
-            if (command.parameters[1].global) npmCommand += " -g --no-save ";
+            if (command.parameters[1].global) npmCommand += " -g ";
             if (command.parameters[1].args) npmCommand += command.parameters[1].args.join(" ") + " ";
             if (command.parameters[1].name) npmCommand += command.parameters[1].name; 
         }
