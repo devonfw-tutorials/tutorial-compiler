fed902a36eaff8c0bc97299748b70760b78df0d3
EduardKrieger <79975016+EduardKrieger@users.noreply.github.com>
2021-03-22 15:18:22 +0100

    Update consoleUtils.ts
    removed Log

--- runners/console/consoleUtils.ts
+++ runners/console/consoleUtils.ts
@@ -21,7 +21,6 @@ export class ConsoleUtils {
 
     static executeCommandAsync(command: string, directory: string, result: RunResult, env: any): child_process.ChildProcess {
         if(result.returnCode != 0) return;
-        console.log(command);
         let process = child_process.spawn(command, [], { shell: true, cwd: directory, env: env });
         if(!process.pid) {
             result.returnCode = 1;
