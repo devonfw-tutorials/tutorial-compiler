0f12394abc7184af3f46ad8b9fefe72e1c9eb445
Eduard Krieger <eduard.krieger@capgemini.com>
2021-03-24 12:59:53 +0100

    functionality added for console runner

--- checkout.ps1
+++ checkout.ps1
@@ -1,7 +1,6 @@
 if(Test-Path "playbooks") {
     Get-ChildItem "playbooks" -Recurse | Remove-Item -Recurse -Force
     Get-Item "playbooks" | Remove-Item -Recurse -Force
-    echo "Halllo"
 }
 
 $TUTORIAL_REPO_URL=(Get-Content .\config.properties | Select-String "tutorials_repo_url").ToString().Split("=")[1]
