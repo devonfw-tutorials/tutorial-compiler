b7f82717c9fad79b0664e1d3f080f84bca5a33cb
EduardKrieger <79975016+EduardKrieger@users.noreply.github.com>
2021-03-31 11:59:06 +0200

    fixed bug with empty lines

--- runners/katacoda/templates/scripts/insert_foreground.sh
+++ runners/katacoda/templates/scripts/insert_foreground.sh
@@ -2,7 +2,7 @@
 FILE="/root/<%= filename; %>"
 PH="##PLACEHOLDER##"
 INSERTED=$(sed -n <%= lineNumber%>p $FILE)
-while [ $INSERTED != $PH ] ;
+while [[ $INSERTED != $PH ]] ;
 do
     sleep 1
     INSERTED=$(sed -n <%= lineNumber%>p $FILE)
