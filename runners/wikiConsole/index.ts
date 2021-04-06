80851af395aed1e30342a89c35bd5aded59349f0
derochs <ochs.marcel@googlemail.com>
2021-04-06 11:20:41 +0200

    Add wikiConsole:cloneRepository

--- runners/wikiConsole/index.ts
+++ runners/wikiConsole/index.ts
@@ -16,7 +16,14 @@ export class WikiConsole extends WikiRunner {
 
     runInstallDevonfwIde(runCommand: RunCommand): RunResult {
         let tools = runCommand.command.parameters[0].join(" ");
-        this.renderWiki(path.join(this.getRunnerDirectory(), "templates", "installDevonfwIde.asciidoc"), { tools: tools })
+        this.renderWiki(path.join(this.getRunnerDirectory(), "templates", "installDevonfwIde.asciidoc"), { tools: tools });
+        return null;
+    }
+
+    runCloneRepository(runCommand: RunCommand): RunResult {
+        let path = runCommand.command.parameters[0];
+        let url = runCommand.command.parameters[1];
+        this.renderWiki(path.join(this.getRunnerDirectory(), "templates", "cloneRepository.asciidoc"), {path: path, url: url});
         return null;
     }
 }
