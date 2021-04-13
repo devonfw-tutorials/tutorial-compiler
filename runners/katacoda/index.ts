6879ab817bba447e0f0f91920f138babac75377b
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2021-04-13 12:54:50 +0200

    added indentations

--- runners/katacoda/index.ts
+++ runners/katacoda/index.ts
@@ -132,12 +132,12 @@ export class Katacoda extends Runner {
 
     runRestoreWorkspace(runCommand: RunCommand): RunResult {
         let workspacesName = "workspace-" + ((runCommand.command.parameters.length > 0 && runCommand.command.parameters[0].workspace)
-        ? runCommand.command.parameters[0].workspace
-        : this.playbookName.replace("/", "").replace(" ","-"));
+            ? runCommand.command.parameters[0].workspace
+            : this.playbookName.replace("/", "").replace(" ","-"));
 
         let workspacesDir = this.getVariable(this.useDevonCommand)
-        ? path.join('/root', "devonfw", "workspaces").replace(/\\/g, "/")
-        : path.join('/root', "workspaces").replace(/\\/g, "/");
+            ? path.join('/root', "devonfw", "workspaces").replace(/\\/g, "/")
+            : path.join('/root', "workspaces").replace(/\\/g, "/");
 
         let user = this.getVariable('user') ? this.getVariable('user') : 'devonfw-tutorials';
         this.renderTemplate(path.join("scripts", "restoreWorkspace.sh"), path.join(this.setupDir, "restoreWorkspace.sh"), {user: user, branch: this.getVariable("branch"), workspace: workspacesName, workspaceDir: workspacesDir, useDevonCommand: !!this.getVariable(this.useDevonCommand)})
