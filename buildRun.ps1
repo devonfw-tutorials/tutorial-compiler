5af65100864f013f0339e9001979821b1ebf9267
Marcel Dießner <marcel.diessner@freenet.de>
2021-04-30 09:59:01 +0200

    removed forced debug mode

--- buildRun.ps1
+++ buildRun.ps1
@@ -4,4 +4,4 @@ Copy-Item -Force -Recurse -Path $PSScriptRoot\playbooks\ -Destination $PSScriptR
 Copy-Item -Force -Recurse -Path $PSScriptRoot\environments\ -Destination $PSScriptRoot\build
 Copy-Item -Force -Recurse -Path $PSScriptRoot\runners\ -Destination $PSScriptRoot\build
 npm test
-node $PSScriptRoot\build\engine\run.js --skipCommands.console dockerCompose $args --debug
+node $PSScriptRoot\build\engine\run.js --skipCommands.console dockerCompose $args
