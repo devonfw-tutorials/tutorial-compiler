4aaaaba9b7ffcac73913e921b409be3e7e2d3587
Marcel Dießner <marcel.diessner@freenet.de>
2021-06-02 16:52:20 +0200

    changed to txt

--- engine/syntax_error_logger.ts
+++ engine/syntax_error_logger.ts
@@ -15,7 +15,7 @@ export class SyntaxErrorLogger {
 
     deactivate() {
         if(this.activated && this.errors.length > 0){
-            fs.writeFileSync(path.join(this.outputDir, "syntaxErrors.md"), 
+            fs.writeFileSync(path.join(this.outputDir, "syntaxErrors.txt"), 
                 "## Function(s) not found: \n - " 
                 + this.errors.join("\n - ")  
                 + "\n \n You can find all supported functions and how to use them [here](https://github.com/devonfw-tutorials/tutorials/wiki/Functions).\n" 
@@ -32,7 +32,7 @@ export class SyntaxErrorLogger {
 
     handleParseError(playbook, error) {
         if(this.activated) { 
-            fs.writeFileSync(path.join(this.outputDir, "syntaxErrors.md"), 
+            fs.writeFileSync(path.join(this.outputDir, "syntaxErrors.txt"), 
                 "## Error while parsing playbook: " 
                 + playbook + 
                 "\n" + "- " 
