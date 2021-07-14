5c4f5322d51151027ca84ed879a36bd74a26c17e
derochs <ochs.marcel@googlemail.com>
2021-07-14 14:11:50 +0200

    Clean up code

--- engine/converter.ts
+++ engine/converter.ts
@@ -9,20 +9,15 @@ import { FileContains } from "../assertions/fileContains";
 
 export class Converter {
 
-private readonly TMP_DIR = "tmp_convert";
+    private readonly TMP_DIR = "tmp_convert";
 
-private platform: ConsolePlatform;
+    private commandsAvailable: boolean;
 
     constructor() {
-        // Determine platform for script execution
-        if(process.platform == "win32") {
-            this.platform = ConsolePlatform.WINDOWS;
-        }else {
-            this.platform = ConsolePlatform.LINUX;
-        }
 
         // Create temporary directory in which files can be created & modified
         this.createTempDir();
+        this.commandsAvailable = this.commandAvailable("asciidoctor -v") && this.commandAvailable("pandoc");
     }
 
     createTempDir(): void {
@@ -40,9 +35,7 @@ private platform: ConsolePlatform;
 
     readTempFile(filename: string): string {
         let file_path = path.join(this.getTempDir(), filename);
-        let fileContent = "";
-        let file = fs.readFileSync(file_path);
-        fileContent = file.toString()
+        let fileContent = fs.readFileSync(file_path, "utf8");
         return fileContent;
     }
 
@@ -63,23 +56,13 @@ private platform: ConsolePlatform;
             this.createTempFile("temp.adoc", asciidocString);
 
             let markdownString = "";
-            if(this.platform == ConsolePlatform.WINDOWS) {
-                if(this.commandAvailable("asciidoctor") && this.commandAvailable("pandoc")) {
-                    this.executeCommand("asciidoctor -b docbook temp.adoc", this.getTempDir());
-                    this.executeCommand("pandoc -f docbook -t gfm temp.xml -o temp.md", this.getTempDir());
-                    markdownString = this.readTempFile("temp.md");
-                }else {
-                    markdownString = this.readTempFile("temp.adoc");
-                } 
+            if(this.commandsAvailable) {
+                this.executeCommand("asciidoctor -b docbook temp.adoc", this.getTempDir());
+                this.executeCommand("pandoc -f docbook -t gfm temp.xml -o temp.md", this.getTempDir());
+                markdownString = this.readTempFile("temp.md");
             }else {
-                if(this.commandAvailable("asciidoctor") && this.commandAvailable("pandoc")) {
-                    this.executeCommand("asciidoctor -b docbook temp.adoc", this.getTempDir());
-                    this.executeCommand("pandoc -f docbook -t gfm temp.xml -o temp.md", this.getTempDir());
-                    markdownString = this.readTempFile("temp.md");
-                }else {
-                    markdownString = this.readTempFile("temp.adoc");
-                }
-            }    
+                markdownString = asciidocString;
+            }     
             return markdownString;
         }        
     }
@@ -94,7 +77,7 @@ private platform: ConsolePlatform;
         let process = child_process.spawnSync(command, {shell: true, cwd: directory});
         if(process.status != 0) {
             console.log("Error executing command: " + command + " (exit code: )" + process.status + ")");
-            console.log(process.stderr.toString(), process.stdout.toString());
+            console.log(process.stderr.toString(), process.toString());
         }
     }
 }
